b3caea2fc0234a1e4198270dc6a8901214798e1a
Wafaa Nasr <wafaa.aanasr@gmail.com>
2019-09-27 16:05:18 +0200

    Add style for menuitems and directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -15,7 +15,10 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, size }) => <MenuItem title={title} imageUrl={imageUrl} size={size} />)}
+                {
+                    this.state.sections.map(({ id, title, imageUrl, size }) =>
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />)
+                }
             </div>
         )
     }
