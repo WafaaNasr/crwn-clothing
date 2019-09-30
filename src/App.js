99a1100b770670f21b103df18d1be2af9b48c2a5
Wafaa Nasr <wafaa.aanasr@gmail.com>
2019-09-30 11:27:13 +0200

    Add HatsPage and   withRouter in the MenuItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homePage';
+import HatsPage from './pages/hatsPage';
 
 function App() {
   return (
     <div >
-        <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
     </div>
   );
 }
