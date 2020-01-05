458fe1cff273ccada5184b12ad8dc62f1e4f8668
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-01-05 18:23:40 +0100

    add styles to collectionItems and customButton

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({ name, price, imageUrl }) => (
 
     <div className='collection-item'>
         <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+        <CustomButton inverted>ADD TO CART</CustomButton>
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
