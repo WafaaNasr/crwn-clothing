c8f27d503850e5e8ba55bbf674fd31d222f48ae4
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-01-25 15:33:24 +0100

    try to fix app.js fot heroku

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shopPage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page';
 import CheckoutPage from './pages/checkout/checkout.component';
+import HomePage from './pages/homePage/homepage.component';
 
 import Header from './components/header/header.component';
 
