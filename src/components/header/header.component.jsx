27f0dc1af075361112684be1132359c24613d9d5
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-01-25 21:50:05 +0100

    use styled-components inside HomePage and Header component with the use of css to write common css for multiple components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utilities";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdpwn.component";
 
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '../../redux/user/user.selector';
-import {selectCartHidden} from '../../redux/cart/cart.selector';
- 
+import { selectCartHidden } from '../../redux/cart/cart.selector';
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
+
 
 const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
+        </LogoContainer>
 
-        <div className='options'>
-            <Link className='option' to='/shop' >SHOP</Link>
-            <Link className='option' to='/' > CONTACT</Link>
+        <OptionsContainer>
+            <OptionLink to='/shop' >SHOP</OptionLink>
+            <OptionLink to='/' > CONTACT</OptionLink>
             {
                 currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>SIGNOUT</div>
+                    <OptionDiv onClick={() => auth.signOut()}>SIGNOUT</OptionDiv>
                     :
-                    <Link className='option' to='/signin' > SIGN IN</Link>
+                    <OptionLink to='/signin' > SIGN IN</OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderContainer>
 );
 const mapStateToProps = createStructuredSelector({ currentUser: selectCurrentUser, hidden: selectCartHidden });
 
