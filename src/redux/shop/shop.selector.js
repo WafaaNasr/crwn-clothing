78d134dfc362a9dcc2567e8b5cab22eec2ebaa65
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-02-02 20:10:24 +0100

    remove shop.data from shop-reducer and  add withSpinner HOC and use it inside the shop page and components

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -7,7 +7,7 @@ const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector([selectShop], shop => shop.collections);
 
-export const selectCollectionForPerview = createSelector([selectShopCollections], collections => Object.values(collections));
+export const selectCollectionForPerview = createSelector([selectShopCollections], collections => collections ? Object.values(collections) : []);
 
 export const selectCollection = collectionUrlParam => createSelector([selectShopCollections],
     collections => collections[collectionUrlParam]
