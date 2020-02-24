939a0c5f56edbc0961532d2c050c23ecae0a86c2
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-02-24 10:10:45 +0100

    configure redux-saga middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './rootReducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 if (process.env.NODE_ENV === 'development')
     middlewares.push(logger);
+
+//sagaMiddleware.run()
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
